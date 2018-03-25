90394ad041608cbd0bcbc5b28019927782abdec1
MARK-42 <ritikgupta15@ymail.com>
2018-03-25 17:01:39 +0530

    first complete fully functional draft

--- app/src/main/java/com/projects/rtk154/justjava2/MainActivity.java
+++ app/src/main/java/com/projects/rtk154/justjava2/MainActivity.java
@@ -7,53 +7,62 @@ import android.widget.TextView;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    int quantity=0;
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    void displayQuantity(int quantity)
-    {
-        TextView tv=(TextView)findViewById(R.id.quantityTv);
-        tv.setText(""+quantity);
+
+    void displayQuantity(int quantity) {
+        TextView tv = (TextView) findViewById(R.id.quantityTv);
+        tv.setText("" + quantity);
     }
-    String name()
-    {
-        EditText ev=(findViewById(R.id.NameEv));
-        String msg=ev.getText().toString();
+
+    String name() {
+        EditText ev = (findViewById(R.id.NameEv));
+        String msg = ev.getText().toString();
         return msg;
     }
-    void orderButton(View view)
-    {
+
+    void orderButton(View view) {
         int price = 10 * quantity;
-        String msg=name();
+        String msg = name();
         displayPrice(price);
         displayMsg(msg);
     }
-    void increment(View view)
-    {
+
+    void increment(View view) {
         quantity++;
         displayQuantity(quantity);
     }
-    void decrement(View view)
-    {
+
+    void decrement(View view) {
         quantity--;
-        if(quantity>0)
+        if (quantity > 0)
             displayQuantity(quantity);
         else {
             displayQuantity(0);
             //add toast
         }
     }
-    void displayPrice(int price)
-    {
-        TextView tv=(TextView)findViewById(R.id.amtTv);
-        tv.setText("Rs. "+price);
+
+    void displayPrice(int price) {
+        TextView tv = (TextView) findViewById(R.id.amtTv);
+        tv.setText("Rs. " + price);
     }
-    void displayMsg(String msg)
+
+    void displayMsg(String msg) {
+        TextView tv = (TextView) findViewById(R.id.messageTv);
+        tv.setText("Thank You for Ordering "+ msg+"\n..Vaelaar Dohairis...");
+    }
+
+    void clear(View view)
     {
-        TextView tv=(TextView)findViewById(R.id.messageTv);
-        tv.setText("Thank You"+msg);
+        TextView tv = (TextView) findViewById(R.id.messageTv);
+        tv.setText("..Vaelar Moghulis..");
+        displayPrice(0);
+        displayQuantity(0);
     }
 }
